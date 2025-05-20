ed1e10762dbcc089061b92fd08214ad69584fe41
RenSafary <102967188+RenSafary@users.noreply.github.com>
2025-05-20 14:25:16 +0500

    S3Client + getting all objs

--- S3-Storage-Interface/MainWindow.xaml.cs
+++ S3-Storage-Interface/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using S3_Storage_Interface.Storage;
 using System.IO;
+using System.Windows.Media.Animation;
 
 namespace S3_Storage_Interface
 {
@@ -25,26 +26,41 @@ namespace S3_Storage_Interface
         public MainWindow()
         {
             InitializeComponent();
+            GetParams();
         }
 
-        public void GetParams()
-        {
-            using (StreamReader sr = new StreamReader("params.txt"))
-            {
-                string bucketName = sr.ReadLine();
-                string endpoint = sr.ReadLine();
-                string accessKey = sr.ReadLine();
-                string secretKey = sr.ReadLine();
-                //string keyName = sr.ReadLine(); in another place
-                //string filePath = sr.ReadLine(); in another place
-
-                Client s3Client = new Client(bucketName, accessKey, secretKey, endpoint, keyName, filePath);
-            }
-        }
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Button_Settings(object sender, RoutedEventArgs e)
         {
             SettingsWindow settingsWindow = new SettingsWindow();
             settingsWindow.Show();
+        }
+        private void Button_Upload(object sender, RoutedEventArgs e)
+        {
+            var openDialogFile = new Microsoft.Win32.OpenFileDialog();
+
+            openDialogFile.Filter = "All files (*.*)|*.*";
+            openDialogFile.Multiselect = true;
+
+            if (openDialogFile.ShowDialog() == true)
+            {
+                string[] filePaths = openDialogFile.FileNames;
+                string[] fileNames = filePaths.Select(path => System.IO.Path.GetFileName(path)).ToArray();
+            }
+        }
+        public void GetParams()
+        {
+            using (StreamReader sr = new StreamReader("params_s3.txt"))
+            {
+                string endpoint = sr.ReadLine();
+                string accessKey = sr.ReadLine();
+                string secretKey = sr.ReadLine();
+                string bucketName = sr.ReadLine();
+
+                Client s3Client = new Client(bucketName, accessKey, secretKey, endpoint);
+
+                GetList getList = new GetList(s3Client);
+                List<string> list = getList.GetAllObjs();
+            }
         }
     }
 }
