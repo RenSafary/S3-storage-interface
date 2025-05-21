4e2310b44e39f45e3da2d7b711588d6626a02a8c
RenSafary <102967188+RenSafary@users.noreply.github.com>
2025-05-21 15:09:54 +0500

    deleting objs

--- S3-Storage-Interface/MainWindow.xaml.cs
+++ S3-Storage-Interface/MainWindow.xaml.cs
@@ -26,7 +26,6 @@ namespace S3_Storage_Interface
         public MainWindow()
         {
             InitializeComponent();
-            GetParams();
         }
 
         private void Button_Settings(object sender, RoutedEventArgs e)
@@ -37,7 +36,6 @@ namespace S3_Storage_Interface
         private void Button_Upload(object sender, RoutedEventArgs e)
         {
             var openDialogFile = new Microsoft.Win32.OpenFileDialog();
-
             openDialogFile.Filter = "All files (*.*)|*.*";
             openDialogFile.Multiselect = true;
 
@@ -45,22 +43,76 @@ namespace S3_Storage_Interface
             {
                 string[] filePaths = openDialogFile.FileNames;
                 string[] fileNames = filePaths.Select(path => System.IO.Path.GetFileName(path)).ToArray();
+
+                List<string> Params = GetParams();
+
+                Client client = new Client(Params[3], Params[1], Params[2], Params[0]);
+                AddToBucket addToBucket = new AddToBucket(client);
+
+                bool response = addToBucket.AddFile(fileNames, filePaths);
             }
         }
-        public void GetParams()
+        public List<string> GetParams()
         {
+            string endpoint;
+            string accessKey;
+            string secretKey;
+            string bucketName;
+
+            List<string> Params = new List<string>();
             using (StreamReader sr = new StreamReader("params_s3.txt"))
             {
-                string endpoint = sr.ReadLine();
-                string accessKey = sr.ReadLine();
-                string secretKey = sr.ReadLine();
-                string bucketName = sr.ReadLine();
+                endpoint = sr.ReadLine();
+                Params.Add(endpoint);
 
-                Client s3Client = new Client(bucketName, accessKey, secretKey, endpoint);
+                accessKey = sr.ReadLine();
+                Params.Add(accessKey);
 
-                GetList getList = new GetList(s3Client);
-                List<string> list = getList.GetAllObjs();
+                secretKey = sr.ReadLine();
+                Params.Add(secretKey);
+
+                bucketName = sr.ReadLine();
+                Params.Add(bucketName);
             }
+            return Params;
+        }
+
+        private void Button_Refresh(object sender, RoutedEventArgs e)
+        {
+            ListAllObjs.Items.Clear();
+            List<string> Params = new List<string>();
+            
+            using(StreamReader sr = new StreamReader("params_s3.txt"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    Params.Add(sr.ReadLine());
+                }
+            }
+            Client client = new Client(Params[3], Params[1], Params[2], Params[0]);
+            GetList getList = new GetList(client);
+
+            List<string> fileNames = getList.GetAllObjs();
+
+            foreach (string file in fileNames)
+            {
+                ListAllObjs.Items.Add(file);
+            }
+        }
+
+        private void Delete_Obj_Click(object sender, RoutedEventArgs e)
+        {
+            string key = ListAllObjs.SelectedItem.ToString();
+
+            List<string> Params = GetParams();
+            Client client = new Client(Params[3], Params[1], Params[2], Params[0]);
+            Delete delete = new Delete(client);
+            delete.DeleteObjs(key);
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
