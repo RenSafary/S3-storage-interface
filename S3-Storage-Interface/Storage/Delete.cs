6d717bad5c21040d8ca38eaf2e02fe63530f07dc
RenSafary <102967188+RenSafary@users.noreply.github.com>
2025-05-21 15:08:38 +0500

    deleting obj

---
+++ S3-Storage-Interface/Storage/Delete.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.S3;
+using Amazon.S3.Model;
+
+namespace S3_Storage_Interface.Storage
+{
+    class Delete
+    {
+        Client _client;
+        public Delete(Client client)
+        {
+            _client = client;
+        }
+        public void DeleteObjs(string key)
+        {
+            var request = new DeleteObjectRequest
+            {
+                BucketName = _client.bucketName,
+                Key = key
+            };
+
+            try
+            {
+                var response = _client.s3Client.DeleteObject(request);
+            }
+            catch(AmazonS3Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
